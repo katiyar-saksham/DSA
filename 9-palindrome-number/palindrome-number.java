d2f0a3d18225ff2e44008ee4937d5cab619aed49
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-07-28 11:23:46 +0530

    Time: 4 ms (100.00%) | Memory: 44.6 MB (11.31%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int OG = x;
+        int reverse = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x /= 10;
+        }
+        return reverse == OG;
+    }
+}
