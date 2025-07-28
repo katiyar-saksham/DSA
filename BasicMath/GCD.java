61895345418022b4030166637c8de9bacc2d1335
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-07-28 13:25:58 +0530

    Auto update

--- BasicMath/GCD.java
+++ BasicMath/GCD.java
@@ -2,14 +2,18 @@ package BasicMath;
 
 public class GCD {
     public static void main(String[] args) {
-        int num1 = 24;
-        int num2 = 36;
+        int N1 = 11;
+        int N2 = 13;
         int gcd = 1;
-        for (int i = 1; i <= Math.min(num1, num2); i++) {
-            if (num1 % i == 0 && num2 % i == 0) {
+        for (int i = Math.min(N1, N2); i > 0; i--) {
+            if (N1 % i == 0 && N2 % i == 0) {
                 gcd = i;
+                break;
             }
         }
-        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        System.out.println("GCD of " + N1 + " and " + N2 + " is: " + gcd);
     }
 }
+
+// Time Complexity: O(min(N1, N2))
+// Space Complexity: O(1)
