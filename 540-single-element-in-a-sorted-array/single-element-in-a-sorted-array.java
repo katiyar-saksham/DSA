4e2dd30a72d28e633201d410ce72d66ca296cdcd
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-08-06 23:14:51 +0530

    Time: 13 ms (5.07%) | Memory: 55 MB (5.01%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        Map<Integer,Integer> mpp = new HashMap<>();
+        for (int num : nums) {
+            mpp.put(num, mpp.getOrDefault(num, 0) + 1);
+        }
+        for (int num : nums) {
+            if (mpp.get(num) == 1) {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
