9497b98677665e610c76ac8961778f77e957e7d5
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-08-10 01:41:41 +0530

    Time: 2 ms (79.77%) | Memory: 46.3 MB (52.00%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,7 +3,7 @@ class Solution {
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                if (i != j) {
+                if (j != i) {
                     int temp = nums[j];
                     nums[j] = nums[i];
                     nums[i] = temp;
