b3775cfde222030e7b008299b50aac0ff5803484
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-08-18 23:40:37 +0530

    Time: 8 ms (82.04%) | Memory: 44.5 MB (11.54%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n <= 0) return false;
+        if (n <= 0)
+            return false;
         while (n % 3 == 0) {
             n /= 3;
         }
