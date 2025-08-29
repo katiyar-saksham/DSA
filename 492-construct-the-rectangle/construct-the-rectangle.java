65733d3d6bd83c350cff6b9635008565dd05fe6c
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-08-30 00:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (50.00%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.java
+++ 492-construct-the-rectangle/construct-the-rectangle.java
@@ -1,18 +1,11 @@
 class Solution {
     public int[] constructRectangle(int area) {
-        int minDiff = Integer.MAX_VALUE;
-        int[] res = new int[2];
+        int W = (int) Math.sqrt(area);
 
-        for (int W = 1; W <= area; W++) {
-            if (area % W == 0) {
-                int L = area / W;
-                if (L >= W && (L - W) < minDiff) {
-                    res[0] = L;
-                    res[1] = W;
-                    minDiff = L - W;
-                }
-            }
+        while (area % W != 0) {
+            W--;
         }
-        return res;
+        int L = area / W;
+        return new int[] { L, W };
     }
 }
