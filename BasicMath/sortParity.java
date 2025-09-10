aba0640b06a789d4198f9dee42588a327dbaec83
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-09-11 00:32:17 +0530

    Auto update

--- BasicMath/sortParity.java
+++ BasicMath/sortParity.java
@@ -1,3 +1,17 @@
+/**
+ * Problem Statement: 
+ * Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+ * Return any array that satisfies this condition.
+ * 
+ * Concept:
+ * The concept used here is two pointers technique. 
+ * We initialize two pointers, left and right, to the start and end of the array respectively.
+ * Then we traverse the array from left to right and if the element at the left pointer is even, we increment the left pointer.
+ * If the element at the left pointer is odd, we swap it with the element at the right pointer and decrement the right pointer.
+ * We repeat this process until the left pointer is greater than the right pointer.
+ * At the end, all the even numbers will be on the left side of the array and all the odd numbers will be on the right side.
+ */
+
 public class sortParity {
     public static int[] sortArrayByParity(int[] A) {
         int left = 0, right = A.length - 1;
--- Training/SpiralMatrix.java
+++ Training/SpiralMatrix.java
@@ -1,100 +1,52 @@
+import java.util.*;
+
 public class SpiralMatrix {
-    // leetcode 54
     public static void main(String[] args) {
-        int[][] nums = { { 1, 2, 3, 4, 5, 6 },
-                { 7, 8, 9, 10, 11, 12 },
-                { 13, 14, 15, 16, 17, 18 },
-                { 19, 20, 21, 22, 23, 24 },
-                { 25, 26, 27, 28, 29, 30 } };
+        int[][] matrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        List<Integer> result = spiralOrder(matrix);
+        System.out.println(result);
+    }
 
-        // boundaries
-        int leftCol = 0, rightCol = nums[0].length - 1;
-        int upRow = 0, botRow = nums.length - 1;
-        int maxElem = (rightCol + 1) * (botRow + 1);
-        int counter = 0;
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
 
-        while (counter < maxElem) {
-            // left to right
-            for (int i = leftCol; i <= rightCol && counter < maxElem; i++) {
-                System.out.print(nums[upRow][i] + " ");
-                counter++;
-            }
-            upRow++;
-            // top to bottom
-            for (int i = upRow; i <= botRow && counter < maxElem; i++) {
-                System.out.print(nums[i][rightCol] + " ");
-                counter++;
-            }
-            rightCol--;
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
 
-            // right to left
-            for (int i = rightCol; i >= leftCol && counter < maxElem; i--) {
-                System.out.print(nums[botRow][i] + " ");
-                counter++;
+        while (top <= bottom && left <= right) {
+            // Traverse left to right
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
             }
-            botRow--;
+            top++;
 
-            // bottom to top
-            for (int i = botRow; i >= upRow && counter < maxElem; i--) {
-                System.out.print(nums[i][leftCol] + " ");
-                counter++;
+            // Traverse top to bottom
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
             }
-            leftCol++;
+            right--;
+
+            // Traverse right to left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // Traverse bottom to top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
         }
+
+        return result;
     }
 }
-
-
-
-
-
-
-
-
-
-// import java.util.ArrayList;
-// import java.util.List;
-
-// class Solution {
-//     public List<Integer> spiralOrder(int[][] matrix) {
-//         List<Integer> result = new ArrayList<>();
-
-//         int top = 0, bottom = matrix.length - 1;
-//         int left = 0, right = matrix[0].length - 1;
-
-//         int maxElem = (bottom + 1) * (right + 1);
-//         int counter = 0;
-
-//         while (counter < maxElem) {
-//             // Traverse left to right
-//             for (int i = left; i <= right && counter < maxElem; i++) {
-//                 result.add(matrix[top][i]);
-//                 counter++;
-//             }
-//             top++;
-
-//             // Traverse top to bottom
-//             for (int i = top; i <= bottom && counter < maxElem; i++) {
-//                 result.add(matrix[i][right]);
-//                 counter++;
-//             }
-//             right--;
-
-//             // Traverse right to left
-//             for (int i = right; i >= left && counter < maxElem; i--) {
-//                 result.add(matrix[bottom][i]);
-//                 counter++;
-//             }
-//             bottom--;
-
-//             // Traverse bottom to top
-//             for (int i = bottom; i >= top && counter < maxElem; i--) {
-//                 result.add(matrix[i][left]);
-//                 counter++;
-//             }
-//             left++;
-//         }
-
-//         return result;
-//     }
-// }
