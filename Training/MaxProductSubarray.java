9d054cac20971e811a24d7074fcf2ebee7d4fe96
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-09-15 00:50:37 +0530

    Auto update

--- Training/MaxProductSubarray.java
+++ Training/MaxProductSubarray.java
@@ -2,5 +2,15 @@ import java.util.*;
 
 public class MaxProductSubarray {
     public static int maxProduct(int[] nums) {
+        int maxProduct = Integer.MIN_VALUE;
+        int currentProduct = 1;
+        for (int i = 0; i < nums.length; i++) {
+            currentProduct *= nums[i];
+            maxProduct = Math.max(maxProduct, currentProduct);
+            if (currentProduct == 0) {
+                currentProduct = 1;
+            }
+        }
+        return maxProduct;
     }
 }
