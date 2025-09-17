ad14fa83553055171dc10571cce7ed6c31f28908
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-09-17 17:56:36 +0530

    Auto update

--- Recusrion/LinearSearch.java
+++ Recusrion/LinearSearch.java
@@ -1,3 +1,16 @@
 public class LinearSearch {
-    
+    static boolean LS(int[] arr, int target, int idx) {
+        if (idx == arr.length - 1) {
+            return false;
+        }
+        if (arr[idx] == target) {
+            return true;
+        }
+        return LS(arr, target, idx + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5 };
+        System.out.println(LS(nums, 1, 0));
+    }
 }
