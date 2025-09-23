a9a0033fa5190f72b5d3aea3e3cf75bb133de9d9
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-09-24 01:31:43 +0530

    Auto update

--- AI Lab/RemovePunctuation.py
+++ AI Lab/RemovePunctuation.py
@@ -3,3 +3,7 @@ import string
 
 def remove_punctuation(input_string):
     return input_string.translate(str.maketrans("", "", string.punctuation))
+
+s = input("Enter the string: ")
+result = remove_punctuation(s)
+print(result)
