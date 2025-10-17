7acc30fbde3f8090046a1198c86bc8da1c253b9a
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-10-17 09:23:46 +0530

    Implemented Kadane’s Algorithm to find the maximum subarray sum in an array of integers.

---
+++ Assessment/MaxSubarraySumKadane.java
@@ -0,0 +1,24 @@
+public class MaxSubarraySumKadane {
+
+    public static void main(String[] args) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int maxSum = maxSubArray(arr);
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+    }
+
+    public static int maxSubArray(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0; // Or throw an exception, depending on problem constraints
+        }
+        int maxSum = nums[0];
+        int currSum = nums[0];
+
+        for (int num : nums) {
+            currSum = Math.max(num, currSum + num);
+            maxSum = Math.max(currSum, maxSum);
+        }
+        return maxSum;
+    }
+
+}
