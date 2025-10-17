de25ecb868b4a2b36610c934212fcbbeb6349013
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-10-17 09:33:13 +0530

    LinearSearch

--- Assessment/FindElementInArray.java
+++ Assessment/FindElementInArray.java
@@ -4,23 +4,16 @@ public class FindElementInArray {
         int[] arr = { 1, 2, 3, 4, 5 };
         int key = 3;
         int position = findElement(arr, key);
-        printResult(position, key);
+        System.out.println(position == -1 ? "Element " + key + " not found" : "Element " + key + " found at position " + position);
     }
 
     private static int findElement(int[] arr, int key) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key) {
-                return i + 1; // return 1-based index
+                return i;
             }
         }
         return -1;
     }
-
-    private static void printResult(int position, int key) {
-        if (position == -1) {
-            System.out.println("Element " + key + " not found");
-        } else {
-            System.out.println("Element " + key + " found at position " + position);
-        }
-    }
 }
+
