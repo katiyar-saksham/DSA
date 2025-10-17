29d152152347a211493f6d0d2635c3676bb8babf
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-10-17 09:52:08 +0530

    pivotIndex

---
+++ Assessment/FindEquilibriumElement.java
@@ -0,0 +1,26 @@
+public class FindEquilibriumElement {
+
+    public static int findEquilibriumElement(int[] nums) {
+        int leftSum = 0;
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+            if (rightSum == leftSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,-4,3,2,1};
+        int result = findEquilibriumElement(nums);
+        System.out.println(result);
+    }
+}
+
