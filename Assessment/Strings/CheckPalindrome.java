f49232495ec6d5f07a8c4e9377f1c4d3e66d7553
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-10-18 22:16:45 +0530

    _

--- Assessment/Strings/CheckPalindrome.java
+++ Assessment/Strings/CheckPalindrome.java
@@ -1,5 +1,29 @@
-package Strings;
+import java.util.Scanner;
 
 public class CheckPalindrome {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = sc.nextLine();
+        if (isPalindrome(s)) {
+            System.out.println(s + " is a palindrome");
+        } else {
+            System.out.println(s + " is not a palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
 }
+
