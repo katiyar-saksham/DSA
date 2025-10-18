640f5aae3fdd65992542b2ebd7aac02068162f6e
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-10-19 00:52:53 +0530

    Check Palindrome

---
+++ Assessment/Strings/CheckPalindrome.java
@@ -0,0 +1,20 @@
+public class CheckPalindrome {
+    public static boolean checkPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPalindrome("MADAM"));
+    }
+}
