cd801f543a3f73aec853fb717944b1f2dca672d5
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-10-19 23:01:53 +0530

    Auto update

--- Assessment/Sorting/BubbleSort.java
+++ Assessment/Sorting/BubbleSort.java
@@ -1,5 +1,29 @@
-package Assessment.Sorting;
-
 public class BubbleSort {
-    
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            boolean isSwapped = false;
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    isSwapped = true;
+                }
+            }
+            if (!isSwapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 8, 12, 3 };
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
