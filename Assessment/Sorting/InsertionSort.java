b5b21898608dc2c635fa9a7365a955a451d18124
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-10-27 00:12:20 +0530

    Auto update

--- Assessment/Sorting/InsertionSort.java
+++ Assessment/Sorting/InsertionSort.java
@@ -1,13 +1,5 @@
 
 public class InsertionSort {
-  private static void swap(int[] arr, int i, int j) {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
-
-  public static void insertionSort(int[] arr) {
-  }
 
   public static void main(String[] args) {
     int[] arr = { 64, 25, 12, 22, 11 };
@@ -16,5 +8,24 @@ public class InsertionSort {
       System.out.print(arr[i] + " ");
     }
     System.out.println();
+  }
+
+  public static void insertionSort(int[] arr) {
+    int n = arr.length;
+    for (int i = 1; i < n - 1; i++) {
+      for (int j = i + 1; j < n - 1; j++) {
+        if (arr[j] < arr[j - 1]) {
+          swap(arr, j, j - 1);
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+  private static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
 }
