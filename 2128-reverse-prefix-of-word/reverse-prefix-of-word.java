adf29ebbf5e7d75e37ee261e5b9e4134b6570204
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2025-11-06 03:19:05 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (6.51%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -2,6 +2,9 @@ class Solution {
     public String reversePrefix(String word, char ch) {
         int idx = word.indexOf(ch);
 
+        if (idx == -1) {
+            return word;
+        }
         StringBuilder sb = new StringBuilder(word.substring(0, idx + 1));
         sb.reverse();
 
