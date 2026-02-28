b8fa7126502e95f41bfcebb6af63726ec9e9bec4
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-02-28 11:36:59 +0530

    Time: 4 ms (100.00%) | Memory: 105.8 MB (96.37%) - LeetSync

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        
+        // agar 3 se kam nodes hain to critical point possible nahi
+        if (head == null || head.next == null || head.next.next == null) {
+            return new int[]{-1, -1};
+        }
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+        int idx = 1;  // curr index 1 pe hai (head index 0 hai) as idx 0 ya n kabhi critical pt nhi ho skte
+
+        int first = -1;
+        int last = -1;
+        int minDist = Integer.MAX_VALUE;
+
+        while (curr.next != null) {
+            ListNode next = curr.next;
+
+            // check kar rahe hain ki curr critical point hai ya nahi
+            // ya to peak hoga ya kmm hoga
+            if ((prev.val < curr.val && curr.val > next.val) ||
+                (prev.val > curr.val && curr.val < next.val)) {
+
+                if (first == -1) {
+                    first = idx;   // pehla critical point fix ho gaya (max distance ke liye)
+                } else {
+                    minDist = Math.min(minDist, idx - last);  // consecutive CP ke beech min distance
+                }
+
+                last = idx;
+            }
+
+            prev = curr;
+            curr = next;
+            idx++;
+        }
+
+        // agar 2 se kam critical points mile to answer -1 -1
+        if (first == -1 || first == last) {
+            return new int[]{-1, -1};
+        }
+
+        return new int[]{minDist, last - first}; //last - first} for MAX Distance
+    }
+}
