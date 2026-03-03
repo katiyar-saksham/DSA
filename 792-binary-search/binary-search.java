8eda25d79101c073fdef80de2e88030e1afc7454
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-03-04 02:21:07 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (85.68%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,21 +1,21 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        return BS(nums, target, 0, nums.length - 1);
-    }
-
-    private int BS(int[] nums, int target, int start, int end) {
-        if (start > end) {
+    private static int helper(int[] nums, int target, int low, int high) {
+        if (low > high)
             return -1;
-        }
 
-        int mid = start + (end - start) / 2;
+        int mid = low + (high - low) / 2;
 
         if (nums[mid] == target) {
             return mid;
         } else if (nums[mid] < target) {
-            return BS(nums, target, mid + 1, end);
+            return helper(nums, target, mid + 1, high);
         } else {
-            return BS(nums, target, start, mid - 1);
+            return helper(nums, target, low, mid - 1);
         }
+    }
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        return helper(nums, target, 0, n - 1);
     }
 }
