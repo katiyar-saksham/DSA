70284b914e804bfe1d900d4f20bfa5a1e35e4115
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-03-19 04:10:33 +0530

    Time: 11 ms (95.79%) | Memory: 47.4 MB (88.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,33 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        //freq map → bucket → reverse traverse → collect k
         int n = nums.length;
-
         HashMap<Integer, Integer> mpp = new HashMap<>();
         for (int num : nums) {
             mpp.put(num, mpp.getOrDefault(num, 0) + 1);
         }
 
         List<Integer>[] bucket = new List[n + 1];
-
         for (int key : mpp.keySet()) {
             int freq = mpp.get(key);
-
             if (bucket[freq] == null) {
                 bucket[freq] = new ArrayList<>();
             }
-
             bucket[freq].add(key);
         }
 
-        int[] res = new int[k];
         int idx = 0;
-
+        int[] res = new int[k];
         for (int i = n; i >= 0 && idx < k; i--) {
             if (bucket[i] != null) {
                 for (int num : bucket[i]) {
                     res[idx++] = num;
-                    if (idx == k)
-                        break;
+                }
+                if (idx == k) {
+                    break;
                 }
             }
         }
