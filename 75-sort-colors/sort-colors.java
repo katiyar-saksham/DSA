d267a686f53ff951894d58e02b7827d94df1ef5d
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-03-19 14:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (20.63%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,24 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int low = 0, mid = 0, high = n - 1;
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                swap(nums, mid, low);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
+        int l = 0;
+        int r = nums.length - 1;
+        int m = l;
+
+        while (m <= r) {
+            if (nums[m] == 0) {
+                swap(nums, m, l);
+                l++;
+                m++;
+            } else if (nums[m] == 1) {
+                m++;
             } else {
-                swap(nums, mid, high);
-                high--;
+                swap(nums, m, r);
+                r--;
             }
         }
     }
 
-    private void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+    private void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
