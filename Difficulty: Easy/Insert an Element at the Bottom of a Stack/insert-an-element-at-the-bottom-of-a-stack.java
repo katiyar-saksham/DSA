fc0d98a39c4ac18315db348c1009258b19f9d74f
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-03-24 11:45:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insert an Element at the Bottom of a Stack/insert-an-element-at-the-bottom-of-a-stack.java
@@ -0,0 +1,17 @@
+class Solution {
+    public Stack<Integer> insertAtBottom(Stack<Integer> st, int x) {
+        Stack<Integer> temp = new Stack<>();
+
+        while (!st.isEmpty()) {
+            temp.push(st.pop());
+        }
+
+        st.push(x);
+
+        while (!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+
+        return st;
+    }
+}
