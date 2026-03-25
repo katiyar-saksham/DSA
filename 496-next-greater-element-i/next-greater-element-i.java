c9fe71a69e1257739324a5950e2acf043044a382
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-03-25 19:35:46 +0530

    Time: 4 ms (73.79%) | Memory: 45.1 MB (72.73%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,36 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> mpp = new HashMap<>();
         Stack<Integer> st = new Stack<>();
-        int n = nums2.length;
-        int[] nge = new int[n];
 
-        // Step 1: Compute NGE for nums2
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && st.peek() <= nums2[i]) {
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums2[i] >= st.peek()) {
                 st.pop();
             }
-
-            nge[i] = st.isEmpty() ? -1 : st.peek();
+            if (st.isEmpty()) {
+                mpp.put(nums2[i], -1);
+            } else {
+                mpp.put(nums2[i], st.peek());
+            }
             st.push(nums2[i]);
         }
 
-        // Step 2: For each element in nums1, find it in nums2
         int[] res = new int[nums1.length];
-
         for (int i = 0; i < nums1.length; i++) {
-            int element = nums1[i];
-
-            // find index in nums2
-            for (int j = 0; j < n; j++) {
-                if (nums2[j] == element) {
-                    res[i] = nge[j];
-                    break;
-                }
-            }
+            res[i] = mpp.get(nums1[i]);
         }
-
         return res;
     }
 }
