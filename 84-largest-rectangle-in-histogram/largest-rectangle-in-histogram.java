ba5b8293403af3d663b671a5a0b30258f1b2cafd
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-03-29 14:36:29 +0530

    Time: 113 ms (9.92%) | Memory: 78 MB (62.60%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -5,47 +5,40 @@ class Solution {
 
         int[] right = new int[n];
         int[] left = new int[n];
+        int maxArea = 0;
 
-        // Next Smaller on Right
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
-            }
-
-            if (st.isEmpty()) {
-                right[i] = n;
-            } else {
-                right[i] = st.peek();
-            }
-
-            st.push(i);
-        }
-
-        st.clear();
-
-        // Next Smaller on Left
+        //left smaller
         for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+            while (!st.isEmpty() && heights[i] <= heights[st.peek()]) {
                 st.pop();
             }
-
             if (st.isEmpty()) {
                 left[i] = -1;
             } else {
                 left[i] = st.peek();
             }
-
             st.push(i);
         }
 
-        int maxArea = 0;
+        st.clear();//MOST IMPT LINE
+
+        //right smaller
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && heights[i] <= heights[st.peek()]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                right[i] = n;
+            } else {
+                right[i] = st.peek();
+            }
+            st.push(i);
+        }
 
         for (int i = 0; i < n; i++) {
             int width = right[i] - left[i] - 1;
-            int area = heights[i] * width;
-            maxArea = Math.max(maxArea, area);
+            maxArea = Math.max(maxArea, width * heights[i]);
         }
-
         return maxArea;
     }
 }
