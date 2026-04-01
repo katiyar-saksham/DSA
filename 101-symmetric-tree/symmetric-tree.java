45bddd196ecb1f36b977b6472338ed0dbc84cb68
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-04-02 01:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (43.16%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return check(root.left, root.right);
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        return check(p.left, q.right) &&
+                check(p.right, q.left);
+    }
+}
