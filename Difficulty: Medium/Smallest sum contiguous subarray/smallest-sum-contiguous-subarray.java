3fb1dbec0e888665d6e3ad127ca7a10a8ac3fded
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-04-16 00:10:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest sum contiguous subarray/smallest-sum-contiguous-subarray.java
+++ Difficulty: Medium/Smallest sum contiguous subarray/smallest-sum-contiguous-subarray.java
@@ -2,14 +2,16 @@
 
 class Solution {
     static int smallestSumSubarray(int a[], int size) {
-        // your code here
+
         int bestEnding = a[0];
         int res = a[0];
-        
-        for(int i=1; i<size;i++){
-            bestEnding=Math.min(bestEnding+a[i],a[i]);
-            res=Math.min(res,bestEnding);
+
+        for(int i = 1; i < size; i++){
+
+            bestEnding = Math.min(a[i], bestEnding + a[i]);
+            res = Math.min(res, bestEnding);
         }
+
         return res;
     }
 }
