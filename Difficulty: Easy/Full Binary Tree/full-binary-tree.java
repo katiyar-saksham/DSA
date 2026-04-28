0fae604de2052970ee3e83d3d85e1b6bd4869094
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-04-28 13:15:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Full Binary Tree/full-binary-tree.java
@@ -0,0 +1,13 @@
+class GfG {
+    boolean isFullTree(Node node) {
+        if (node == null) return true;
+        
+        if (node.left == null && node.right == null) return true;
+        
+        if (node.left != null && node.right != null) {
+            return isFullTree(node.left) && isFullTree(node.right);
+        }
+        
+        return false;
+    }
+}
