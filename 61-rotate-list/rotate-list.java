631c1bde520f3f25215f585aa5d7619485ed0e84
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-05-05 23:58:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (25.10%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -1,7 +1,10 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0)
+
+        // Reverse-based approach use karke linked list rotate ki hai (array jaisa concept)
+        if (head == null || head.next == null || k == 0) {
             return head;
+        }
 
         int len = 0;
         ListNode temp = head;
@@ -10,23 +13,43 @@ class Solution {
             temp = temp.next;
         }
 
-        k = k % len;
-        if (k == 0)
+        k %= len;
+        if (k == 0) {
             return head;
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        for (int i = 0; i < k; i++) {
-            fast = fast.next;
         }
-        while (fast.next != null) {
-            slow = slow.next;
-            fast = fast.next;
+
+        head = reverse(head);
+
+        ListNode first = head;
+        ListNode curr = head;
+        for (int i = 1; i < k; i++) {
+            curr = curr.next;
         }
-        ListNode a = slow.next;
-        slow.next = null;
-        fast.next = head;
-        return a;
+
+        ListNode second = curr.next;
+        curr.next = null;
+
+        first = reverse(first);
+        second = reverse(second);
+
+        ListNode t = first;
+        while (t.next != null)
+            t = t.next;
+        t.next = second;
+
+        return first;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode p = null;
+        ListNode c = head;
+
+        while (c != null) {
+            ListNode n = c.next;
+            c.next = p;
+            p = c;
+            c = n;
+        }
+        return p;
     }
 }
