71e53ebc4b98d8c696ff38dcd35cf89285f39a42
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-05-10 16:42:12 +0530

    Time: 7 ms (98.51%) | Memory: 48 MB (19.39%) - LeetSync

--- 4131-minimum-flips-to-make-binary-string-coherent/minimum-flips-to-make-binary-string-coherent.java
+++ 4131-minimum-flips-to-make-binary-string-coherent/minimum-flips-to-make-binary-string-coherent.java
@@ -1,30 +1,33 @@
 class Solution {
-
     public int minFlips(String s) {
-
         int n = s.length();
-
-        int one = 0;
-        int zero = 0;
+        int one = 0, zero = 0;
 
         for (char ch : s.toCharArray()) {
-            if (ch == '0') {
-                zero++;
-            } else {
+            if (ch == '1') {
                 one++;
+            } else {
+                zero++;
             }
         }
 
-        int ans = 0;
+        int ans = Math.min(one, zero);
 
-        if (one >= 1) {
-            ans = Math.min(zero, one - 1);
+        if (one <= 1 || one == 0 || zero == 0) {
+            return 0;
+        } else {
+            if (s.charAt(0) == '1' && s.charAt(n - 1) == '1') {
+                if (one == 2)
+                    return 0;
+                else
+                    return Math.min(one - 2, zero);
+            }
         }
+        return Math.min(zero, one - 1);
+        // if (s.charAt(0) == '1' && s.charAt(n - 1) == '1' && zero == n - 2) {
+        //     return 0;
+        // }
 
-        if (n > 1 && s.charAt(0) == '1' && s.charAt(n - 1) == '1') {
-            ans = Math.min(ans, one - 2);
-        }
-
-        return ans;
+        // return Math.min(one - 1, zero);
     }
 }
