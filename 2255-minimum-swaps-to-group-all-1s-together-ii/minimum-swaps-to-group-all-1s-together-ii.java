4020d67d7244f883b91b20a82484cb42a81da1ca
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-06-10 12:12:05 +0530

    Time: 9 ms (52.36%) | Memory: 90.6 MB (31.70%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,26 +1,38 @@
 class Solution {
     public int minSwaps(int[] nums) {
-        int ones = 0;
-        for (int x : nums) {
-            if (x == 1) ones++;
-        }
-
-        if (ones <= 1) return 0;
         int n = nums.length;
-        int curr = 0;
+        int o = 0;
+        int z = 0;
+        int res = Integer.MAX_VALUE;
 
-        for (int i = 0; i < ones; i++) {
-            curr += nums[i % n];
+        for (int num : nums) {
+            if (num == 1) {
+                o++;
+            }
+        }
+        if (o <= 1) {
+            return 0;
+        }
+        if (o == 1) {
+            return 1;
         }
 
-        int maxOnes = curr;
-        for (int i = ones; i < n + ones; i++) {
-            curr -= nums[(i - ones) % n];
-            curr += nums[i % n];
-
-            maxOnes = Math.max(maxOnes, curr);
+        for (int i = 0; i < o; i++) {
+            if (nums[i % n] == 0) {
+                z++;
+            }
+        }
+        res = z;
+        for (int i = o; i < n + o; i++) {
+            if (nums[i % n] == 0) {
+                z++;
+            }
+            if (nums[(i - o) % n] == 0) {
+                z--;
+            }
+            res = Math.min(res, z);
         }
 
-        return ones - maxOnes;
+        return res;
     }
 }
