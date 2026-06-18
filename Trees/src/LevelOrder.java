70b545920c880eecb38fceb56869aa30c9c45f05
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-06-18 16:23:39 +0530

    Auto update

---
+++ Trees/src/LevelOrder.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+class Node {
+    int val;
+    Node left, right;
+
+    Node(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
+
+public class LevelOrder {
+
+    public static void levelOrderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+            System.out.print(curr.val + " ");
+
+            if (curr.left != null) {
+                q.offer(curr.left);
+            }
+            if (curr.right != null) {
+                q.offer(curr.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+        levelOrderTraversal(root);
+    }
+}
