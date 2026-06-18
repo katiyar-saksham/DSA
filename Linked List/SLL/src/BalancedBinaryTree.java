56fcae9f095e0df3b0942754282f2406689c14a5
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-06-18 12:49:09 +0530

    Auto update

--- Linked List/SLL/src/BalancedBinaryTree.java
+++ Linked List/SLL/src/BalancedBinaryTree.java
@@ -14,6 +14,29 @@ class Solution {
         if (arr.length == 0 || arr[0] == -1) {
             return null;
         }
+
+        Node root = new Node(arr[0]);
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+
+        int i = 1;
+        while (!q.isEmpty() && i < arr.length) {
+            Node curr = q.poll();
+
+            if (i < arr.length && arr[i] != -1) {
+                curr.left = new Node(arr[i]);
+                q.offer(curr.left);
+            }
+            i++;
+
+            if (i < arr.length && arr[i] != -1) {
+                curr.right = new Node(arr[i]);
+                q.offer(curr.right);b
+            }
+            i++;
+        }
+
+        return root;
     }
 
     public static boolean isBalanced(Node root) {
