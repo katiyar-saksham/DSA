821172ea081821be016e9dc6398d16c43a4138c3
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-07-02 21:40:31 +0530

    Time: 3 ms (56.90%) | Memory: 47.4 MB (10.93%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,7 +3,7 @@ class Solution {
         HashMap<Integer, Integer> mpp = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (mpp.containsKey(target - nums[i])) {
-                return new int[] {mpp.get(target - nums[i]), i };
+                return new int[] { mpp.get(target - nums[i]), i };
             }
 
             mpp.put(nums[i], i);
