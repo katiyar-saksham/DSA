a32310df03cd9e1e548c9d15f3c364efee1893ba
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-07-14 02:41:33 +0530

    Time: 1 ms (28.11%) | Memory: 42.5 MB (22.09%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> res = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= 9; i++) {
+            q.offer(i);
+        }
+
+        while (!q.isEmpty()) {
+            int num = q.poll();
+
+            if (num >= low && num <= high) {
+                res.add(num);
+            }
+
+            int dgt = num % 10;
+
+            if (dgt < 9) {
+                int next = num * 10 + (dgt + 1);
+                if (next <= high) {
+                    q.offer(next);
+                }
+            }
+        }
+        Collections.sort(res);
+        return res;
+    }
+}
