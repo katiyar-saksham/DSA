2ad08ec350a9fbda97c4640b73b17d5a734326fc
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-07-27 18:34:21 +0530

    Convert BitManipulation from submodule to regular directory

---
+++ BitManipulation/BitManipulation.java
@@ -0,0 +1,11 @@
+// package BitManipulation;
+
+// import java.util.*;
+
+public class BitManipulation {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 9;
+        System.out.println(a & b);
+    }
+}
