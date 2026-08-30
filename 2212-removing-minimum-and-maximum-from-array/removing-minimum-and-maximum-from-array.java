86d4a6d0b6c895dbfae601b6729a148499de5a3d
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-08-30 20:55:52 +0530

    Time: 3 ms (61.82%) | Memory: 86.9 MB (8.73%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -1,29 +1,25 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
         int n = nums.length;
-
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        int minIdx = -1;
-        int maxIdx = -1;
+        int min = 0;
+        int mx = 0;
+        int res = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
-            if (nums[i] < min) {
-                min = nums[i];
-                minIdx = i;
-            }
-            if (nums[i] > max) {
-                max = nums[i];
-                maxIdx = i;
+            if (nums[i] < nums[min]) {
+                min = i;
+            } else if (nums[i] > nums[mx]) {
+                mx = i;
             }
         }
 
-        // Step 2: Try all 3 ways
-        int opt1 = Math.max(minIdx, maxIdx) + 1;                  // rm both from left
-        int opt2 = n - Math.min(minIdx, maxIdx);                  // rm both from right
-        int opt3 = (Math.min(minIdx, maxIdx) + 1) + (n - Math.max(minIdx, maxIdx)); // rm one from each side
+        int left = Math.max(min, mx) + 1;
+        int right = n - Math.min(min, mx);
+        int leftRight = (min + 1) + (n - mx);
+        int rightLeft = (mx + 1) + (n - min);
 
-        // Step 3: Return the best (minimum moves)
-        return Math.min(opt1, Math.min(opt2, opt3));
+        return Math.min(
+                Math.min(left, right),
+                Math.min(leftRight, rightLeft));
     }
 }
