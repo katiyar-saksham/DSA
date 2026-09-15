49cd3577e3618e9904cef0b3e18d8b7b71de7101
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-09-16 02:20:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.java
@@ -1,28 +1,40 @@
+/* Structure of Binary Tree Node
+class Node {
+	int data;
+	Node left, right;
+	
+	Node(int val) {
+		this.data = val;
+		this.left = this.right = null;
+	}
+} */
+
 class Solution {
-    public ArrayList<Integer> leftView(Node root) {
-        ArrayList<Integer> res = new ArrayList<>();
-        
-        if(root == null) return res;
-        
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            
-            for(int i = 0; i < size; i++){
-                Node curr = q.poll();
-                
-                // LEFT VIEW LOGIC
-                if(i == 0){
-                    res.add(curr.data);
-                }
-                
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
-            }
-        }
-        
-        return res;
-    }
+	public ArrayList<Integer> leftView(Node root) {
+		ArrayList<Integer> lst = new ArrayList<>();
+		
+		if (root == null) {
+			return lst;
+		}
+		
+		Queue<Node> q = new LinkedList<>();
+		q.add(root);
+		
+		while (!q.isEmpty()) {
+			int size = q.size();
+			
+			lst.add(q.peek().data);
+			
+			for (int i = 0; i<size; i++) {
+				Node val = q.poll();
+				if (val.left != null) {
+					q.add(val.left);
+				}
+				if (val.right != null) {
+					q.add(val.right);
+				}
+			}
+		}
+		return lst;
+	}
 }
